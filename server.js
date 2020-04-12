ce605c29af9f8307ae7549229d65893bc9fc9437
Gevrai Jodoin-Tremblay <gejo@kaloom.com>
2020-04-11 21:05:02 -0400

    heroku deployment test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var app = express()
 
 // Set a static folder
-app.use(express.static('../html5-export'))
+app.use(express.static('html5-export'))
 
 var PORT = process.env.PORT || 8000
 var server = app.listen(PORT, () => {
